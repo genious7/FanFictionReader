729ee9235789839836304dc799ce1ffdc7b9f11c
Michael Chen <michaelchentejada@gmail.com>
2013-12-23 21:49:11 -0500

    List stories now works. Parser works.

--- src/com/gmail/michaelchentejada/fanfictionreader/Parser.java
+++ src/com/gmail/michaelchentejada/fanfictionreader/Parser.java
@@ -7,18 +7,25 @@ import java.util.HashMap;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
-import android.util.Log;
-
 public class Parser {
 	
 	public static final String TITLE = "Title";
+	public static final String URL = "Url";
 	public static final String VIEWS = "Views";
 	public static final String VIEWS_INT = "Views Int";
-	public static final String URL = "Url";
-	public static final String SUMMARY = "Summary";
-	public static final String FOLLOWS = "Follows";
+	
 	public static final String AUTHOR = "Author";
+	public static final String AUTHOR_URL = "Author Url";
+	public static final String SUMMARY = "Summary";
+	public static final String RATING = "Rating";
+	public static final String LANGUAGUE = "Languague";
+	public static final String GENRE = "Genre";	
+	public static final String CHAPTER = "Chapter";
 	public static final String LENGHT = "Lenght";
+	public static final String FAVORITES = "Favorites";
+	public static final String FOLLOWS = "Follows";
+	public static final String UPDATED = "Updated";
+	public static final String PUBLISHED = "Published";
 	
 	/**
 	 * Obtains the list of sub-categories inside one of the main categories.
@@ -54,23 +61,66 @@ public class Parser {
 	public static ArrayList<HashMap<String, String>> Stories(String url){	
 		try {
 			org.jsoup.nodes.Document document = Jsoup.connect(url).get();
-			Elements titles = document.select("div.z-list  > a.stitle");
-			Elements authors = document.select("div.z-list  > a[href^=/u/]");
-			Elements summaries = document.select("div.z-list  > div.z-padtop");
-			Elements attribs = document.select("div.z-list > div.z-padtop > div.z-padtop2");
+			Elements summaries = document.select("div#content > div.bs");
+			Elements titles = summaries.select("a[href~=(?i)/s/\\d+/1/.*]");
+			Elements authors = summaries.select("a[href^=/u/]");
+			
+			Elements attribs = summaries.select("div.gray");
 			
 			ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String,String>>();
 			
 			for (int i = 0; i < titles.size(); i++) {
 				HashMap<String, String> TempMap = new HashMap<String, String>();
+				
 				TempMap.put(TITLE, titles.get(i).ownText());
+				
 				TempMap.put(URL, titles.get(i).attr("href"));
 				
-				Log.d("Parse", titles.get(i).ownText());
-				Log.d("Parse", authors.get(i).ownText());
-				Log.d("Parse", summaries.get(i).ownText());
-				Log.d("Parse", attribs.get(i).ownText());
+				TempMap.put(AUTHOR, authors.get(i).ownText());
 				
+				TempMap.put(AUTHOR_URL, authors.get(i).attr("href"));
+				
+				TempMap.put(SUMMARY, summaries.get(i).ownText().replaceFirst("(?i)by\\s*", ""));
+
+				String attrib[] = attribs.get(i).text().split("\\s*,\\s*");
+				
+				TempMap.put(RATING, attrib[0]);		
+				TempMap.put(LANGUAGUE,attrib[1]);
+				int j = 2;
+				if (!(attrib[j].contains("chapter")||attrib[j].contains("words"))) {
+					TempMap.put(GENRE,attrib[j]);
+					j++;
+				}else{
+					TempMap.put(GENRE,"None");
+				}
+				if (attrib[j].contains("chapter")) {
+					TempMap.put(CHAPTER,attrib[j].replaceFirst("(?i)chapters:\\s*", ""));
+					j++;
+				}else{
+					TempMap.put(CHAPTER,"1");
+				}
+				TempMap.put(LENGHT,attrib[j].replaceFirst("(?i)words:\\s*", ""));
+				j++;
+				if (attrib[j].contains("favs")) {
+					TempMap.put(FAVORITES,attrib[j].replaceFirst("(?i)favs:\\s*", ""));
+					j++;
+				}else{
+					TempMap.put(FAVORITES,"0");
+				}
+				if (attrib[j].contains("follows")) {
+					TempMap.put(FOLLOWS,attrib[j].replaceFirst("(?i)follows:\\s*", ""));
+					j++;
+				}else{
+					TempMap.put(FOLLOWS,"0");
+				}
+				if (attrib[j].contains("updated")) {
+					attrib = attrib[j].replaceFirst("(?i)updated:\\s*", "").split("(?i)published:\\s*");
+					TempMap.put(UPDATED,attrib[0]);
+					TempMap.put(PUBLISHED,attrib[1]);
+				}else{
+					TempMap.put(UPDATED,attrib[j].replaceFirst("\\s*", ""));
+					TempMap.put(PUBLISHED,attrib[j].replaceFirst("\\s*", ""));
+				}
 				list.add(TempMap);
 			}
 			return list;
--- src/com/gmail/michaelchentejada/fanfictionreader/StoryMenu.java
+++ src/com/gmail/michaelchentejada/fanfictionreader/StoryMenu.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnCancelListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +22,10 @@ public class StoryMenu extends Activity {
 	private ListView listView;
 	private Context context;
 	private ProgressDialog progressDialog;
+	
 	private ArrayList<HashMap<String, String>> list;
+	private ParseStories asyncTask = new ParseStories();
+	
 	private final OnItemClickListener listListener = new OnItemClickListener() {
 
 		@Override
@@ -29,7 +35,13 @@ public class StoryMenu extends Activity {
 			
 		}
 	};
+
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putSerializable("List", list);
+		super.onSaveInstanceState(outState);
+	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,23 +51,80 @@ public class StoryMenu extends Activity {
 		listView = (ListView)findViewById(R.id.storyMenuListView);
 		listView.setOnItemClickListener(listListener);
 		
+		String url = "https://m.fanfiction.net" + getIntent().getStringExtra("URL");
+		
 		if (savedInstanceState == null) {
-			
+			asyncTask.execute(url);
 		} else {
-			SimpleAdapter adapter;
-			
+			list = (ArrayList<HashMap<String, String>>) savedInstanceState.getSerializable("List");
+			SimpleAdapter adapter = new SimpleAdapter(context, list,
+					R.layout.story_menu_list_item, new String[] {
+							Parser.TITLE, Parser.SUMMARY, Parser.AUTHOR,
+							Parser.LENGHT, Parser.FOLLOWS }, new int[] {
+							R.id.story_menu_list_item_title,
+							R.id.story_menu_list_item_summary,
+							R.id.story_menu_list_item_author,
+							R.id.story_menu_list_item_words,
+							R.id.story_menu_list_item_follows });
+			listView.setAdapter(adapter);
 		}
 		
 	}
 	
 	private class ParseStories extends AsyncTask<String, Void, ArrayList<HashMap<String, String>>>{
-
+		@Override
+		protected void onPreExecute() {
+			progressDialog = new ProgressDialog(context);
+			progressDialog.setTitle("");
+			progressDialog.setMessage(getResources().getString(R.string.dialog_loading));
+			progressDialog.setOnCancelListener(new OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					finish();	
+				}
+			});
+			progressDialog.show();
+			super.onPreExecute();
+		}
 		@Override
 		protected ArrayList<HashMap<String, String>> doInBackground(
-				String... params) {
-			// TODO Auto-generated method stub
-			return null;
+				String... url) {
+			return Parser.Stories(url[0]);
 		}
+		@Override
 		
+		protected void onPostExecute(ArrayList<HashMap<String, String>> result) {
+			progressDialog.dismiss();
+			
+			if (result != null) {
+				list = result;
+				SimpleAdapter adapter = new SimpleAdapter(context, list,
+						R.layout.story_menu_list_item, new String[] {
+								Parser.TITLE, Parser.SUMMARY, Parser.AUTHOR,
+								Parser.LENGHT, Parser.FOLLOWS }, new int[] {
+								R.id.story_menu_list_item_title,
+								R.id.story_menu_list_item_summary,
+								R.id.story_menu_list_item_author,
+								R.id.story_menu_list_item_words,
+								R.id.story_menu_list_item_follows });
+				listView.setAdapter(adapter);
+				super.onPostExecute(result);
+			}else{
+				end(getResources().getString(R.string.dialog_internet));
+			}
+		}
+	}
+	
+	private void end(String error){
+		Intent end = new Intent();
+		end.putExtra("Error", error);
+		setResult(RESULT_CANCELED, end);
+		finish();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		asyncTask.cancel(true);
+		super.onDestroy();
 	}
 }
