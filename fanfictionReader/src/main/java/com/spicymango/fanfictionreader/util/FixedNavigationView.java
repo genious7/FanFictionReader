83ac0cc1db48ee8078f9ecc4af847b9d806fba75
Michael Chen <michaelchentejada@gmail.com>
2015-12-30 22:38:48 -0500

    Fixed BarParcelableException on android version 2.3-3.0 See https://code.google.com/p/android/issues/detail?id=196430

---
+++ fanfictionReader/src/main/java/com/spicymango/fanfictionreader/util/FixedNavigationView.java
@@ -0,0 +1,36 @@
+package com.spicymango.fanfictionreader.util;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.design.widget.NavigationView;
+import android.util.AttributeSet;
+
+/**
+ * Fixes BadParcelableException on Android versions 2.3-3.0
+ * See https://code.google.com/p/android/issues/detail?id=196430
+ */
+public class FixedNavigationView extends NavigationView{
+	public FixedNavigationView(Context context) {
+		super(context);
+	}
+
+	public FixedNavigationView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public FixedNavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	final protected void onRestoreInstanceState(Parcelable savedState) {
+		if (savedState != null) {
+			SavedState state = (SavedState) savedState;
+
+			if (state.menuState != null)
+				state.menuState.setClassLoader(getContext().getClass().getClassLoader());
+		}
+
+		super.onRestoreInstanceState(savedState);
+	}
+}
