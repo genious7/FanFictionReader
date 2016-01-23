genious7/FanFictionReader

commit 2eb74aab96ddb0f599316d387d131a7228285042

Characters are now displayed on the details dialog Minor typos fixed throughout code

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/LibraryDownloader.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.SparseArray;
 
@@ -66,7 +67,7 @@ public class LibraryDownloader extends IntentService{
 			+ "(?i)\\ARated: Fiction ([KTM]\\+?) - "//Rating
 			+ "([^-]+) - "//language
 			+ "(?:([^ ]+) - )?"//Genre
-			+ "(?:(?!(?>Chapters))(?:(?! - ).)++ - )?"//characters (non capturing)
+			+ "(?:(?!Chapters)([^-]+(?:(?<=Jenny )- [^-]+)?) - )?"//characters (non capturing)
 			+ "(?:Chapters: (\\d++) - )?" //Chapters
 			+ "Words: ([\\d,]++) - " //Words
 			+ "(?:Reviews: [\\d,]++ - )?"//Reviews (non capturing)
@@ -141,7 +142,7 @@ public class LibraryDownloader extends IntentService{
 						return Result.NO_CHANGE;
 					}
 					
-					totalPages = story.getChapterLenght();
+					totalPages = story.getChapterLength();
 				
 					//If an update exists and incremental updating is enabled, update chapters as needed
 					if (Settings.isIncrementalUpdatingEnabled(this)) {
@@ -262,10 +263,17 @@ public class LibraryDownloader extends IntentService{
 		builder.setRating(matcher.group(1));
 		builder.setLanguage(matcher.group(2));		
 		if (matcher.group(3) != null) builder.setGenre(matcher.group(3));
-		if (matcher.group(4) != null) builder.setChapterLenght(Parser.parseInt(matcher.group(4)));
-		builder.setWordLenght(Parser.parseInt(matcher.group(5)));
-		if (matcher.group(6) != null) builder.setFavorites(Parser.parseInt(matcher.group(6)));
-		if (matcher.group(7) != null) builder.setFollows(Parser.parseInt(matcher.group(7)));
+		if (matcher.group(4) != null){
+			String[] characterArray = matcher.group(4).split("([,\\[\\]] ?)++");
+			for (String character : characterArray) {
+				if (!TextUtils.isEmpty(character))
+					builder.addCharacter(character);
+			}
+		}
+		if (matcher.group(5) != null) builder.setChapterLength(Parser.parseInt(matcher.group(5)));
+		builder.setWordLength(Parser.parseInt(matcher.group(6)));
+		if (matcher.group(7) != null) builder.setFavorites(Parser.parseInt(matcher.group(7)));
+		if (matcher.group(8) != null) builder.setFollows(Parser.parseInt(matcher.group(8)));
 		builder.setUpdateDate(updateDate);		
 		builder.setPublishDate(publishDate);
 		builder.setCompleted(attribs.text().contains("Complete"));

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/activity/AccountActivity.java
@@ -244,7 +244,7 @@ public class AccountActivity extends AppCompatActivity {
 				builder.scheme(getString(R.string.fanfiction_scheme))
 						.authority(getString(R.string.fanfiction_authority))
 						.appendEncodedPath("u")
-						.appendEncodedPath(story.getAuthor_id() + "")
+						.appendEncodedPath(story.getAuthorId() + "")
 						.appendEncodedPath("");
 				Intent i = new Intent(getActivity(), AuthorMenuActivity.class);
 				i.setData(builder.build());

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/activity/AuthorMenuActivity.java
@@ -423,7 +423,7 @@ public class AuthorMenuActivity extends AppCompatActivity{
 					builder.setAuthor(mAuthor);
 					builder.setAuthorId(mAuthorId);
 					builder.setSummary(element.ownText().replaceFirst("(?i)by\\s*", ""));
-					builder.setFanFicAttribs(attribs.text());
+					builder.setFanFicAttributes(attribs.text());
 					builder.setUpdateDate(updateDate);
 					builder.setPublishDate(publishDate);
 					builder.setCompleted(complete);

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/activity/LibraryMenuActivity.java
@@ -119,7 +119,7 @@ public class LibraryMenuActivity extends AppCompatActivity implements LoaderCall
 					new Thread(new Runnable() {
 						@Override
 						public void run() {
-							int length = story.getChapterLenght();
+							int length = story.getChapterLength();
 							getContentResolver().delete(databaseUri, null, null);
 							FileHandler.deleteStory(LibraryMenuActivity.this, story.getId());
 						}
@@ -136,7 +136,7 @@ public class LibraryMenuActivity extends AppCompatActivity implements LoaderCall
 			builder.scheme(getString(R.string.fanfiction_scheme))
 					.authority(getString(R.string.fanfiction_authority))
 					.appendEncodedPath("u")
-					.appendEncodedPath(story.getAuthor_id() + "")
+					.appendEncodedPath(story.getAuthorId() + "")
 					.appendEncodedPath("");
 			Intent i = new Intent(this, AuthorMenuActivity.class);
 			i.setData(builder.build());

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/dialogs/DetailDialog.java
@@ -14,9 +14,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.net.Uri.Builder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
@@ -34,7 +36,6 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 	private final static String EXTRA_AUTHOR = "AUTHOR";
 	
 	private Story mStory;
-	private boolean mShowAuthor;
 	
 	private final static int[] formats = {
 				R.string.detail_author,
@@ -49,19 +50,21 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 				R.string.detail_updated,
 				R.string.detail_published,
 				R.string.detail_story_id,
-				R.string.detail_complete
+				R.string.detail_complete,
+				R.string.detail_characters
 	};
 	
 	public DetailDialog(){
 	}
 	
 	@SuppressLint("InflateParams")
+	@NonNull
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		super.onCreateDialog(savedInstanceState);
 		
 		mStory = getArguments().getParcelable(EXTRA_STORY);
-		mShowAuthor = getArguments().getBoolean(EXTRA_AUTHOR);
+		boolean showAuthor = getArguments().getBoolean(EXTRA_AUTHOR);
 
 		int vPad = getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin);
 		int hPad = getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
@@ -78,16 +81,17 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 				mStory.getAuthor(),
 				mStory.getCategory(),
 				mStory.getRating(),
-				mStory.getlanguage(),
+				mStory.getLanguage(),
 				mStory.getGenre(),
-				Integer.toString(mStory.getChapterLenght()),
-				mStory.getWordLenght(),
+				Integer.toString(mStory.getChapterLength()),
+				mStory.getWordLength(),
 				mStory.getFavorites(),
 				mStory.getFollows(),
 				DateFormat.getDateInstance().format(mStory.getUpdated()),
 				DateFormat.getDateInstance().format(mStory.getPublished()),
 				Long.toString(mStory.getId()),
-				mStory.isCompleted() ? getString(R.string.complete_true) : getString(R.string.complete_false)
+				mStory.isCompleted() ? getString(R.string.complete_true) : getString(R.string.complete_false),
+				TextUtils.join(", ", mStory.getCharacters())
 		};
 		
 		for (int i = 0; i < formats.length; i++) {
@@ -98,7 +102,7 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 			}
 		}
 		
-		if (mShowAuthor) {
+		if (showAuthor) {
 			Button authorBtn = new Button(getActivity());
 			authorBtn.setText(R.string.menu_navigation_browse_by_author);
 			authorBtn.setOnClickListener(this);
@@ -106,9 +110,9 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 			layout.addView(authorBtn);
 		}
 		
-		AlertDialog diag= builder.create();
-		diag.setCanceledOnTouchOutside(true);
-		return diag;
+		AlertDialog dialog= builder.create();
+		dialog.setCanceledOnTouchOutside(true);
+		return dialog;
 	}
 	
 	@Override
@@ -118,7 +122,7 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 		builder.scheme(getString(R.string.fanfiction_scheme))
 				.authority(getString(R.string.fanfiction_authority))
 				.appendEncodedPath("u")
-				.appendEncodedPath(mStory.getAuthor_id() + "")
+				.appendEncodedPath(mStory.getAuthorId() + "")
 				.appendEncodedPath("");
 		Intent i = new Intent(getActivity(), AuthorMenuActivity.class);
 		i.setData(builder.build());
@@ -126,17 +130,17 @@ public class DetailDialog extends DialogFragment implements OnClickListener{
 		
 	}
 	
-	public static final void show(FragmentActivity fragmentActivity, Story story) {
+	public static void show(FragmentActivity fragmentActivity, Story story) {
 		boolean showAuthor = true;
 		if (fragmentActivity instanceof AuthorMenuActivity || fragmentActivity instanceof LibraryMenuActivity) {
 			showAuthor = false;
 		}
-		DialogFragment diag = new DetailDialog();
+		DialogFragment dialog = new DetailDialog();
 		Bundle bundle = new Bundle();
 		bundle.putParcelable(EXTRA_STORY, story);
 		bundle.putBoolean(EXTRA_AUTHOR, showAuthor);
-		diag.setArguments(bundle);
-		diag.show(fragmentActivity.getSupportFragmentManager(), null);
+		dialog.setArguments(bundle);
+		dialog.show(fragmentActivity.getSupportFragmentManager(), null);
 	}
 	
 }

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/menu/storymenu/StoryMenuAdapter.java
@@ -70,10 +70,10 @@ public class StoryMenuAdapter extends ArrayAdapter<Story> {
 			holder.title.setText(getItem(position).getName());
 			holder.summary.setText(getItem(position).getSummary());
 			holder.author.setText(getItem(position).getAuthor());
-			holder.author.setTag(getItem(position).getAuthor_id());
-			holder.words.setText(String.format(words, getItem(position).getWordLenght()));
+			holder.author.setTag(getItem(position).getAuthorId());
+			holder.words.setText(String.format(words, getItem(position).getWordLength()));
 			holder.follows.setText(String.format(follows, getItem(position).getFollows()));
-			holder.chapters.setText(String.format(chapters, getItem(position).getChapterLenght()));
+			holder.chapters.setText(String.format(chapters, getItem(position).getChapterLength()));
 
 		    return convertView;
 	}

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/menu/storymenu/StoryMenuLoaders.java
@@ -103,7 +103,7 @@ final class StoryMenuLoaders {
 				// Add characters if they exist
 				Elements characters = story.select("li.characters > a");
 				for (Element character : characters) {
-					builder.addCharacters(character.ownText());
+					builder.addCharacter(character.ownText());
 				}
 
 				// Fetch the language
@@ -114,14 +114,14 @@ final class StoryMenuLoaders {
 				// Fetch the number of words
 				Element words = story.select("dd.words").first();
 				if (words == null) return false;
-				builder.setWordLenght(Parser.parseInt(words.ownText()));
+				builder.setWordLength(Parser.parseInt(words.ownText()));
 
 				// Fetch the number of chapters
 				Element chapters = story.select("dd.chapters").first();
 				if (chapters == null) return false;
 				String chapterNo = chapters.text();
 				chapterNo = chapterNo.substring(0, chapterNo.indexOf('/'));
-				builder.setChapterLenght(Integer.parseInt(chapterNo));
+				builder.setChapterLength(Integer.parseInt(chapterNo));
 
 				// Fetch the number of hits (follows)
 				Element follows = story.select("dd.hits").first();

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/provider/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
 	
-	private static final int DATABASE_VERSION = 7; //Database version 7
+	private static final int DATABASE_VERSION = 8; //Database version 8
 	private static final String DATABASE_NAME = "library.db";
 	
 	//The name of the FanFiction table
@@ -24,7 +24,7 @@ public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
             + KEY_AUTHOR_ID + " TEXT,"
             + KEY_RATING + " TEXT,"
             + KEY_GENRE + " TEXT,"
-            + KEY_LANGUAGUE + " TEXT," 
+            + KEY_LANGUAGE + " TEXT,"
             + KEY_CATEGORY + " TEXT,"
             + KEY_CHAPTER + " INTEGER,"
             + KEY_LENGTH + " INTEGER,"
@@ -35,7 +35,8 @@ public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
             + KEY_SUMMARY + " TEXT NOT NULL,"
             + KEY_LAST + " INTEGER,"
             + KEY_COMPLETE + " BOOLEAN,"
-            + KEY_OFFSET + " INTEGER"
+            + KEY_OFFSET + " INTEGER,"
+			+ KEY_CHARACTERS + " TEXT"
             + ")";
 	
 	@Override
@@ -53,6 +54,8 @@ public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
 			onCreate(db);
 		}
 		if (oldVersion < 4) {
+			// In this version, the internal representation of lengths, favorites, and follows was
+			// changed from text entries to integer entries
 			db.execSQL("UPDATE " + FANFICTION_TABLE + " SET " + KEY_LENGTH + " = REPLACE(" + KEY_LENGTH+ ",',','')");
 			db.execSQL("UPDATE " + FANFICTION_TABLE + " SET " + KEY_FAVORITES + " = REPLACE(" + KEY_FAVORITES+ ",',','')");
 			db.execSQL("UPDATE " + FANFICTION_TABLE + " SET " + KEY_FOLLOWERS + " = REPLACE(" + KEY_FOLLOWERS+ ",',','')");
@@ -64,7 +67,7 @@ public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
 	                + KEY_AUTHOR_ID + " TEXT,"
 	                + KEY_RATING + " TEXT,"
 	                + KEY_GENRE + " TEXT,"
-	                + KEY_LANGUAGUE + " TEXT," 
+	                + KEY_LANGUAGE + " TEXT,"
 	                + KEY_CATEGORY + " TEXT,"
 	                + KEY_CHAPTER + " INTEGER,"
 	                + KEY_LENGTH + " INTEGER,"
@@ -74,19 +77,26 @@ public class DatabaseHelper extends SQLiteOpenHelper  implements SqlConstants{
 	                + KEY_UPDATED + " INTEGER,"
 	                + KEY_SUMMARY + " TEXT NOT NULL,"
 	                + KEY_LAST + " INTEGER"
-	                + ")");
+					+ ")");
 			db.execSQL("INSERT INTO tmp SELECT * FROM " + FANFICTION_TABLE);
 			db.execSQL("DROP TABLE " + FANFICTION_TABLE);
 			db.execSQL("ALTER TABLE tmp RENAME TO " + FANFICTION_TABLE);
 		}
 		if (oldVersion < 5) {
+			// A column was added in order to remember if a story was completed or not
 			db.execSQL("ALTER TABLE " + FANFICTION_TABLE + " ADD COLUMN " + KEY_COMPLETE + " BOOLEAN DEFAULT 0");
 		}
 		if (oldVersion < 6) {
-			db.execSQL("ALTER TABLE " + FANFICTION_TABLE + " ADD COLUMN " + KEY_OFFSET + " INTEGER DEFAULT 0");
+			// A column was added in order to remember the position of the scroll bar in the story
+			db.execSQL("ALTER TABLE " + FANFICTION_TABLE + " ADD COLUMN " + KEY_OFFSET + " TEXT DEFAULT ''");
 		}
 		if (oldVersion < 7) {
+			// A FictionPress table was defined.
 			db.execSQL("CREATE TABLE " + FICTIONPRESS_TABLE + TABLE_DEF);
+		}
+		if (oldVersion < 8){
+			// A column for the different characters was added
+			db.execSQL("ALTER TABLE " + FANFICTION_TABLE + " ADD COLUMN " + KEY_CHARACTERS + " TEXT DEFAULT ''");
 		}
 	}
 

commit 4157f41e54692041510fe018e7ba075928d2a5ce

Improved regex attribute parsing
Fixes bug regarding viewing characters with a dash, such as "Obi-Wan". The bug was introduced in 2eb74aab96ddb0f599316d387d131a7228285042.

fanfictionReader/src/main/java/com/spicymango/fanfictionreader/LibraryDownloader.java
@@ -67,12 +67,12 @@ public class LibraryDownloader extends IntentService{
 			+ "(?i)\\ARated: Fiction ([KTM]\\+?) - "//Rating
 			+ "([^-]+) - "//language
 			+ "(?:([^ ]+) - )?"//Genre
-			+ "(?:(?!Chapters)([^-]+(?:(?<=Jenny )- [^-]+)?) - )?"//characters (non capturing)
-			+ "(?:Chapters: (\\d++) - )?" //Chapters
-			+ "Words: ([\\d,]++) - " //Words
-			+ "(?:Reviews: [\\d,]++ - )?"//Reviews (non capturing)
-			+ "(?:Favs: ([\\d,]++) - )?"//favorites
-			+ "(?:Follows: ([\\d,]++))?"); //Follows
+			+ "(?:(?!Chapters)((?:(?! - ).)+?(?:(?<=Jenny) - [^-]+)?) - )?"//characters (non capturing)
+			+ "(?>Chapters: (\\d+) - )?" //Chapters
+			+ "Words: ([\\d,]+) - " //Words
+			+ "(?>Reviews: [\\d,]+ - )?"//Reviews (non capturing)
+			+ "(?>Favs: ([\\d,]+) - )?"//favorites
+			+ "(?>Follows: ([\\d,]+))?"); //Follows
 	
 	/**
 	 * Describes the author id Url pattern