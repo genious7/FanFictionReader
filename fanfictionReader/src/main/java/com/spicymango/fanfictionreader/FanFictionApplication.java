c6e4460c6771722c22afb7f36dab2dd6515fe1f7
Michael Chen <michaelchentejada@gmail.com>
2017-10-25 01:05:27 -0500

    The app now properly caches the menus instead of reloading them.

--- fanfictionReader/src/main/java/com/spicymango/fanfictionreader/FanFictionApplication.java
+++ fanfictionReader/src/main/java/com/spicymango/fanfictionreader/FanFictionApplication.java
@@ -5,6 +5,9 @@ import android.preference.PreferenceManager;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
+import com.spicymango.fanfictionreader.menu.DataFragment;
+
+import java.io.File;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -28,5 +31,38 @@ public class FanFictionApplication extends Application {
 
 		//Initialize settings to default values upon the first access to the application
 		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+		// Clear the cache folder on app start
+		clearSavedInstanceStateCacheDirectory();
+	}
+
+	/**
+	 * Clears all the savedInstanceState cache files. If the cache folder does not exist (such as
+	 * in a new install), this function will create the cache folder.
+	 * <p>
+	 * These files are used to store data instead of
+	 * the savedInstanceState bundle whenever the data may exceed 1 MB, which is the limit before
+	 * android Nougat throws a TransactionTooLargeException.
+	 * </p>
+	 * <p>
+	 * The cache folder is cleared every time the app restarts in order to prevent it from growing
+	 * too large. The DataFragment deletes cache files when destroyed. However, since the onDestroy()
+	 * method is not guaranteed to be called, the cache files are also cleared here.
+	 * </p>
+	 */
+	private void clearSavedInstanceStateCacheDirectory() {
+		final File cacheFolder = new File(getCacheDir(), DataFragment.SAVED_INSTANCE_STATE_CACHE_PATH);
+
+		if (cacheFolder.exists()) {
+			// If the directory exists, clear all cache files.
+			for (File file : cacheFolder.listFiles()){
+				if (!file.isDirectory()){
+					file.delete();
+				}
+			}
+		} else {
+			// If the directory does not exist, create it
+			cacheFolder.mkdir();
+		}
 	}
 }
--- fanfictionReader/src/main/java/com/spicymango/fanfictionreader/menu/DataFragment.java
+++ fanfictionReader/src/main/java/com/spicymango/fanfictionreader/menu/DataFragment.java
@@ -1,36 +1,99 @@
 package com.spicymango.fanfictionreader.menu;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
+import com.crashlytics.android.Crashlytics;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * A fragment used to store the data obtained from the server during state changes. This is used
  * instead of the savedInstanceState bundle in order to prevent an
  * android.os.android.os.TransactionTooLargeException on android Nougat.
+ * <p>
+ * Whenever the app is closed, any saved data is written to a cached file in order to recover it
+ * upon reopening the app.
  * </p>
  * Created by Michael Chen on 15/09/2017.
  */
 
 public final class DataFragment extends Fragment {
-	private final static String KEY_DATA = "KEY_DATA";
+	/**
+	 * Any cache files that represent a saved instance state are stored in this folder.
+	 */
+	public final static String SAVED_INSTANCE_STATE_CACHE_PATH = "Saved Instance State";
+
+	/**
+	 * A simple variable to indicate that data has been cached. This is required in order to
+	 * determine if a cached file is relevant to the current session.
+	 */
+	private final static String KEY_FLAG = "KEY_FLAG";
+
+	/** A bundle containing the retained data during orientation changes */
 	private Bundle mSavedData;
+
+	/** The cache file*/
+	private File mCacheFile;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// In order to retain data, the fragment must not be recreated on instance changes.
-		// SetRetainInstance not available on api 11, which is why we'll resort to using
-		// savedInstanceState in there anyways. In api 11, large bundles fail silently
-		// so it isn't a problem.
+		// Create the filename. The filename is composed of the class name (to easily identify it)
+		// the intent uri hashcode (to distinguish the Category and Sub-Category Menus and the
+		// .tmp extension to clearly mark the file as a temporary file.
+		final File cacheFolder = new File(getActivity().getCacheDir(), SAVED_INSTANCE_STATE_CACHE_PATH);
+		String filename = getActivity().getClass().getName();
+		final Uri uri = getActivity().getIntent().getData();
+		if (uri != null){
+			filename += uri.hashCode();
+		}
+		filename += ".tmp";
+		mCacheFile = new File(cacheFolder, filename);
+
+		// Prevent the fragment from being recreated on configuration changes.
 		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			setRetainInstance(true);
-		} else if (savedInstanceState != null) {
-			// If api is less than 11, the fragment is not saved but the os wont throw a
-			// TransactionTooLargeException.
-			mSavedData = savedInstanceState.getBundle(KEY_DATA);
+		}
+
+		// If the fragment is being recreated, read the cache file
+		if (savedInstanceState != null && savedInstanceState.containsKey(KEY_FLAG)) {
+			// Read the cache file
+			final Parcel bundledParcel = Parcel.obtain();
+			FileInputStream fin = null;
+
+			try {
+				// The cache file may not exist if an error occurred while saving the file
+				if (mCacheFile.exists()) {
+					fin = new FileInputStream(mCacheFile);
+					byte[] buffer = new byte[(int) fin.getChannel().size()];
+					fin.read(buffer, 0, buffer.length);
+					fin.close();
+
+					bundledParcel.unmarshall(buffer, 0, buffer.length);
+					bundledParcel.setDataPosition(0);
+					mSavedData = bundledParcel.readBundle(getClass().getClassLoader());
+				}
+			} catch (IOException e) {
+				Crashlytics.logException(e);
+			} finally {
+				bundledParcel.recycle();
+
+				// Close the FileInputStream. No try-with-resources below api 19.
+				if (fin != null) {
+					try {
+						fin.close();
+					} catch (IOException e) {/**/}
+				}
+			}
 		}
 	}
 
@@ -44,12 +107,54 @@ public final class DataFragment extends Fragment {
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
+		// On android Honeycomb or greater, the fragment will be retained on configuration changes
+		// and therefore, the data can be retrieved from the fragment directly. The cache file only
+		// needs to be used on Api versions lower than Honeycomb or when a configuration change is
+		// not occurring.
+		if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB ||
+				!getActivity().isChangingConfigurations()) {
 
-		// Save the bundle on api less than 11.
-		if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-			outState.putBundle(KEY_DATA, mSavedData);
+			// Write the cache file
+			FileOutputStream fos = null;
+			final Parcel bundledParcel = Parcel.obtain();
+
+			try {
+				fos = new FileOutputStream(mCacheFile);
+
+				// Convert the bundle to a parcel and save it on the cache file
+				mSavedData.writeToParcel(bundledParcel, 0);
+				fos.write(bundledParcel.marshall());
+
+				// Data has been saved. Mark a flag.
+				outState.putBoolean(KEY_FLAG, true);
+
+				fos.flush();
+			} catch (IOException e) {
+				Crashlytics.logException(e);
+			} finally {
+				bundledParcel.recycle();
+
+				// Close the FileOutputStream. No try-with-resources below api 19.
+				if (fos != null) {
+					try {
+						fos.close();
+					} catch (IOException e) {/**/}
+				}
+			}
 		}
+
+		super.onSaveInstanceState(outState);
 	}
 
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		// If the activity is being closed, delete the cache file
+		if (getActivity().isFinishing()) {
+			if (mCacheFile.exists()) {
+				mCacheFile.delete();
+			}
+		}
+	}
 }
